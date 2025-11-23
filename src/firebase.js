13c217833e55fb5e72b1dca8e59db07be945892e
Circle.S <greatnuegochi1425@gmail.com>
2025-11-23 21:20:16 +0900

    config fixed

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// ▼ 여기 따옴표 안에 .env 파일 내용을 복사해서 넣으세요 ▼
 const firebaseConfig = {
   apiKey: "AIzaSyAml5COo4-mvDgpzNHGNPxOJ13ed6KuXFU",
   authDomain: "humanbgm.firebaseapp.com",
@@ -16,7 +13,6 @@ const firebaseConfig = {
   appId: "1:915431573625:web:1721a29800da4e1e30eca8"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
